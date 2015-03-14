7080888baa3a7c6a116c3fbf77b20f761a716d32
Margarita Oysolova <m.oysolova@gmail.com>
2015-03-14 20:17:14 +0200

    For and While

---
+++ week_2/2.List_Problems/while_traverse.py
@@ -0,0 +1,3 @@
+books = ["Learn You a Haskell", "The Healthy Programmer",
+         "Code Complete", "The Pragmatic Programmer",
+         "Pro Git", "Introduction to Algorithms", "Concrete Mathematics"]
