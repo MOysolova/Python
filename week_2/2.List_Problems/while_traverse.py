12e4cd35931f2f003915eb7e7b639428276f16bf
Margarita Oysolova <m.oysolova@gmail.com>
2015-03-14 20:21:09 +0200

    Update While

--- week_2/2.List_Problems/while_traverse.py
+++ week_2/2.List_Problems/while_traverse.py
@@ -1,3 +1,9 @@
 books = ["Learn You a Haskell", "The Healthy Programmer",
          "Code Complete", "The Pragmatic Programmer",
          "Pro Git", "Introduction to Algorithms", "Concrete Mathematics"]
+n = len(books) - 1
+index = 0
+while index <= n:
+    book = books[index]
+    index += 1
+    print(book)
